9253a524cf50f03f165d09205d1895d576d9e182
Praveen-Cyber311 <nagapraveen311@gmail.com>
2025-02-28 12:36:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,38 @@
 from flask import Flask, jsonify
 from flask_cors import CORS
-from itertools import product
+import random
 from collections import Counter
 
 app = Flask(__name__)
 CORS(app)
 
-def compute_probabilities():
-    die_A = [1, 2, 3, 4, 5, 6]
-    die_B = [1, 2, 3, 4, 5, 6]
-    
-    sums = [a + b for a, b in product(die_A, die_B)]
-    sum_counts = Counter(sums)
-    probabilities = {k: v / 36 for k, v in sum_counts.items()}
-    
-    return sum_counts, probabilities
+def roll_dice():
+    return random.randint(1, 6), random.randint(1, 6)
 
-@app.route('/compute_probabilities', methods=['GET'])
-def get_probabilities():
-    sum_counts, probabilities = compute_probabilities()
-    return jsonify({"sum_counts": sum_counts, "probabilities": probabilities})
+def get_distribution():
+    outcomes = [(i, j) for i in range(1, 7) for j in range(1, 7)]
+    sum_counts = Counter(sum(pair) for pair in outcomes)
+    probabilities = {k: v / 36 for k, v in sum_counts.items()}
+    return probabilities
+
+def undoom_dice():
+    new_die_a = [1, 2, 3, 4, 1, 2]
+    new_die_b = [7 - x for x in new_die_a]
+    return new_die_a, new_die_b
+
+@app.route('/roll', methods=['GET'])
+def roll():
+    die_a, die_b = roll_dice()
+    return jsonify({'Die A': die_a, 'Die B': die_b, 'Sum': die_a + die_b})
+
+@app.route('/probabilities', methods=['GET'])
+def probabilities():
+    return jsonify(get_distribution())
+
+@app.route('/undoom', methods=['GET'])
+def undoom():
+    new_die_a, new_die_b = undoom_dice()
+    return jsonify({'New Die A': new_die_a, 'New Die B': new_die_b})
 
 if __name__ == '__main__':
     app.run(debug=True)
